14fba8ce5cce241a87edb88bbcbf894338f98e78
Łukasz Wołodkiewicz <wolodkiewiczlukasz@gmail.com>
2022-12-28 15:24:02 +0100

    Migration from compose to Dockerfile - nginx.conf WIP

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,16 +4,18 @@ import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Todo } from './todos/entities/todo.entity';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TodosModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'db',
+      host: process.env.DB_HOST || 'mysql',
       port: 3306,
-      username: 'root',
-      password: 'example',
-      database: 'db',
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || 'example',
+      database: process.env.DB_NAME || 'db',
       entities: [Todo],
       synchronize: true,
       autoLoadEntities: true,
--- frontend/src/store/todo.ts
+++ frontend/src/store/todo.ts
@@ -3,7 +3,7 @@ import { defineStore } from "pinia";
 import axios from "axios";
 
 const connector = axios.create({
-  baseURL: "http://localhost:3000/todos",
+  baseURL: "todos",
 });
 
 type Todo = {
